48645f836d3d2b5a86ab5d1ab5218e9cc3373296
Marcelo Javier García <marcejgarcia@gmail.com>
2024-09-23 16:50:52 -0300

    Update Trabajo Practico N1.ts

--- Trabajo Practico N1.ts
+++ Trabajo Practico N1.ts
@@ -1,26 +1,29 @@
 console.clear();
 import * as fs from 'readline-sync';
 /////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 let participante: number = fs.questionInt("Ingrese cantidad de participantes: ");
-////////determinar puntaje entre 0 y 5
+//////////determinar puntaje entre 0 y 5/////////////////////
+/////////////////////////////////////////////////////////////
 let determinarPuntaje = (a:number): void =>  {
     while (a < 0 && a >5 )
     {
         console.log("El valor debe ser entre mayor que 0 y menor o igual a 5");
     };
 }
-//calcula el puntaje
+///////////////////calcula el puntaje//////////////////////////////
+///////////////////////////////////////////////////////////////////
 let calcularPuntaje = (a: number, b: number, c: number): number => {
     let suma: number = (a + b + c);
     return suma;
 };
-///////////////////////////////////////////////////////////////////
-//determina ganador
-let determinarGanador = (): any => {
+/////////////////////determina ganador/////////
+//////////////////////////////////////////////
+
+let determinarGanador = (): void => {
     let notaMayor:number=0;
     let ganador:any=0; 
     for (let i: number = 1; i <= participante; i++) {
-    let particOrden = i
     console.log("Notas particitante",i);
     let sabor:number = fs.questionInt("ingrese puntaje por sabor: ");
     determinarPuntaje(sabor);
@@ -29,13 +32,12 @@ let determinarGanador = (): any => {
     let dificultad: number = fs.questionInt("ingrese puntaje por dificultad: ");
     determinarPuntaje(dificultad);
     let pteNota = calcularPuntaje(sabor, presentacion, dificultad);
-    if (pteNota > notaMayor) {
+    if (pteNota >= notaMayor) {
         notaMayor = pteNota;
         ganador = i;
-    } else {ganador ="empate"}
-}
+    } else {ganador ="empate"};
+};
     return ganador;
 }
-////////////////////////////////////////////////////////////////////
 let ganador = determinarGanador()
 console.log("el ganador de la competencia es",ganador);
