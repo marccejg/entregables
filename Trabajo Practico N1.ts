15d3a4911c656368b803c02d318e726801e63eac
Marcelo Javier García <marcejgarcia@gmail.com>
2024-09-23 14:12:01 -0300

    Update Trabajo Practico N1.ts

--- Trabajo Practico N1.ts
+++ Trabajo Practico N1.ts
@@ -3,36 +3,46 @@ import * as fs from 'readline-sync';
 
 
 /////////////////////////////////////////////////////////////
+
 let participante: number = fs.questionInt("Ingrese cantidad de participantes: ");
+
+////////determinar puntaje entre 0 y 5
+let determinarPuntaje = (a:number): void =>  {
+    while (a <= 0 && a >=5 )
+    {
+        console.log("El valor debe ser entre mayor que 0 y menor o igual a 5");
+    };
+}
 //calcula el puntaje
 let calcularPuntaje = (a: number, b: number, c: number): number => {
     let suma: number = (a + b + c);
     return suma;
 };
+
 ///////////////////////////////////////////////////////////////////
 //determina ganador
 
-
-
 let determinarGanador = (): any => {
     let notaMayor:number=0;
-   
-    
+    let ganador:number=0; 
     for (let i: number = 1; i <= participante; i++) {
     let particOrden = i
-    let sabor: number = fs.questionInt("ingrese puntaje por sabor: ");
+    console.log("Notas particitante",i);
+    let sabor:number = fs.questionInt("ingrese puntaje por sabor: ");
+    determinarPuntaje(sabor);
     let presentacion: number = fs.questionInt("ingrese puntaje por presentacion: ");
+    determinarPuntaje(presentacion);
     let dificultad: number = fs.questionInt("ingrese puntaje por dificultad: ");
+    determinarPuntaje(dificultad);
     let pteNota = calcularPuntaje(sabor, presentacion, dificultad);
     if (pteNota > notaMayor) {
         notaMayor = pteNota;
-    }else {notaMayor};
-  
+        ganador = i;
+    }else {
+        notaMayor;
+    };  
 }
-    return notaMayor;
-
-
-
+    return ganador;
 }
 let ganador = determinarGanador()
-console.log(ganador);
+console.log("El ganadro es el participanta Nº",ganador);
