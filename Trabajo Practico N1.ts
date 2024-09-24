204b733d6b48b9959558cb3a055d7df4c724ff3a
Marcelo Javier García <marcejgarcia@gmail.com>
2024-09-23 21:37:41 -0300

    Update Trabajo Practico N1.ts

--- Trabajo Practico N1.ts
+++ Trabajo Practico N1.ts
@@ -5,38 +5,52 @@ import * as fs from 'readline-sync';
 let participante: number = fs.questionInt("Ingrese cantidad de participantes: ");
 //////////determinar puntaje entre 0 y 5/////////////////////
 /////////////////////////////////////////////////////////////
-let determinarPuntaje = (a:number): void =>  {
-    while (a < 0 && a >5 )
-    {
-        console.log("El valor debe ser entre mayor que 0 y menor o igual a 5");
-    };
-}
+
+
 ///////////////////calcula el puntaje//////////////////////////////
 ///////////////////////////////////////////////////////////////////
 let calcularPuntaje = (a: number, b: number, c: number): number => {
     let suma: number = (a + b + c);
     return suma;
 };
+
+
 /////////////////////determina ganador/////////
 //////////////////////////////////////////////
 
 let determinarGanador = (): void => {
-    let notaMayor:number=0;
-    let ganador:any=0; 
+    let notaMayor: number = 0;
+    let ganador: any = 0;
+
     for (let i: number = 1; i <= participante; i++) {
-    console.log("Notas particitante",i);
-    let sabor:number = fs.questionInt("ingrese puntaje por sabor: ");
-    determinarPuntaje(sabor);
-    let presentacion: number = fs.questionInt("ingrese puntaje por presentacion: ");
-    determinarPuntaje(presentacion);
-    let dificultad: number = fs.questionInt("ingrese puntaje por dificultad: ");
-    determinarPuntaje(dificultad);
-    let pteNota = calcularPuntaje(sabor, presentacion, dificultad);
-    if (pteNota > notaMayor) {
-        notaMayor = pteNota;
-        ganador = i;
-    } else {ganador ="empate"};
-};
+        console.log("Notas particitante", i);
+
+
+        let sabor: number = fs.questionInt("ingrese puntaje por sabor: ")
+        while (sabor < 0 || sabor > 5) {
+            sabor = fs.questionInt("ingrese puntaje por sabor: ");
+        };
+        let presentacion: number = fs.questionInt("ingrese puntaje por presentacion: ");
+        while (presentacion < 0 || presentacion > 5) {
+            presentacion = fs.questionInt("ingrese puntaje por presentacion: ");
+        };
+        let dificultad: number = fs.questionInt("ingrese puntaje por dificultad: ");
+        while (dificultad < 0 || dificultad > 5) {
+            dificultad = fs.questionInt("ingrese puntaje por dificultad: ");
+        };
+        let pteNota = calcularPuntaje(sabor, presentacion, dificultad);
+
+        let notaMayor: number = 0;
+        if (pteNota > notaMayor) {
+            notaMayor = pteNota;
+            ganador = i;
+        } else { ganador = "empate" };
+    };
+    return ganador;
+}
+let ganador = determinarGanador()
+console.log("el ganador de la competencia es", ganador);
+
     return ganador;
 }
 let ganador = determinarGanador()
